13a2446ed32622fb4eb58b1574b5e656863f0003
Sergey Karayev <sergeykarayev@gmail.com>
2018-07-31 22:22:07 -0700

    adding all conv

--- text_recognizer/networks/__init__.py
+++ text_recognizer/networks/__init__.py
@@ -1,7 +1,6 @@
 from .mlp import mlp
 # Hide lines below until Lab 2
 from .cnn import lenet, lenet_all_conv
-# from .line_cnn_fixed_width import LineCnnFixedWidth
 from .line_cnn_sliding_window import line_cnn_sliding_window
-# from .line_cnn_all_conv import LineCnnAllConv
+from .line_cnn_all_conv import line_cnn_all_conv
 # Hide lines above until Lab 2
--- text_recognizer/networks/line_cnn.py
+++ text_recognizer/networks/line_cnn.py
@@ -31,18 +31,19 @@ def line_cnn_sliding_window(
     image_reshaped = Reshape((image_height, image_width, 1))(image_input)
     # (image_height, image_width, 1)
 
-    image_patches = Lambda(
-        slide_window,
-        arguments={'window_width': window_width, 'window_stride': window_stride}
-    )(image_reshaped)
-    # (num_windows, image_height, window_width, 1)
+    # Conv block
+    conv1 = Conv2D(32, kernel_size=(3, 3), activation='relu')(image_reshaped)
+    conv2 = Conv2D(64, (3, 3), activation='relu')(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv2)
+    # (image_height // 2 - 2, image_width // 2 - 2)
 
-    # Make a LeNet and get rid of the top softmax layer and preceding dropout
-    convnet = lenet((image_height, window_width, 1), (num_classes,))
-    convnet = Model(inputs=convnet.inputs, outputs=convnet.layers[-2].output)
+    # Conv block
+    conv3 = Conv2D(32, kernel_size=(3, 3), activation='relu')(pool1)
+    conv4 = Conv2D(64, (3, 3), activation='relu')(conv3)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv4)
+    #
 
-    convnet_outputs = TimeDistributed(convnet)(image_patches)
-    # (num_windows, 128)
+
 
     convnet_outputs_extra_dim = Lambda(lambda x: tf.expand_dims(x, -1))(convnet_outputs)
     # (num_windows, 128, 1)
@@ -52,7 +53,7 @@ def line_cnn_sliding_window(
     # Note that it will likely produce too many items in the output sequence, so take only output_length.
 
     # Your code below here (Lab 2)
-    num_windows = int((image_width - window_width) / window_stride) + 1
+    num_windows = int((image_width - window_width) / window_stride) + 2
     width = int(num_windows / output_length)
 
     conved_convnet_outputs = Conv2D(num_classes, (width, 128), (width, 1), activation='softmax')(convnet_outputs_extra_dim)
