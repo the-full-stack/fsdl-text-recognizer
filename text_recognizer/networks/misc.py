3698d1666f0f921fe99136a934f3f81b8465d527
Saurabh Bipin Chandra <srbhchandra@gmail.com>
2018-08-02 18:36:19 -0700

    minor
    Signed-off-by: Saurabh Bipin Chandra <srbhchandra@gmail.com>

--- text_recognizer/networks/misc.py
+++ text_recognizer/networks/misc.py
@@ -8,9 +8,8 @@ def slide_window(image, image_height, window_width, window_stride):
     Returns (num_windows, image_height, window_width, 1) output, where
     num_windows is floor((image_width - window_width) / window_stride) + 1
     """
-    height = 1
-    kernel = [1, height, window_width, 1]
-    strides = [1, height, window_stride, 1]
+    kernel = [1, 1, window_width, 1]
+    strides = [1, 1, window_stride, 1]
     patches = tf.extract_image_patches(image, kernel, strides, [1, 1, 1, 1], 'VALID')
     patches = tf.transpose(patches, (0, 2, 1, 3))
 
