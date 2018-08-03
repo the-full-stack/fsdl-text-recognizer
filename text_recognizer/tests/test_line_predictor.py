e1e3e9e38a56969f1d0f030f2fdff243113e9c7b
Saurabh Bipin Chandra <srbhchandra@gmail.com>
2018-08-02 20:33:27 -0700

    Changes to lab 3
    Signed-off-by: Saurabh Bipin Chandra <srbhchandra@gmail.com>

--- text_recognizer/tests/test_line_predictor.py
+++ text_recognizer/tests/test_line_predictor.py
@@ -15,6 +15,18 @@ class TestEmnistLinePredictor(unittest.TestCase):
         predictor = LinePredictor()
 
         for filename in SUPPORT_DIRNAME.glob('*.png'):
+            pred, conf = predictor.predict(str(filename))
+            true = filename.stem
+            edit_distance = editdistance.eval(pred, true) / len(pred)
+            print(f'Pred: "{pred}" | Confidence: {conf} | True: {true} | Edit distance: {edit_distance}')
+            self.assertLess(editdistance.eval(pred, filename), 0.2)
+
+
+class TestEmnistLinePredictorVariableImageWidth(unittest.TestCase):
+    def test_filename(self):
+        predictor = LinePredictor()
+
+        for filename in SUPPORT_DIRNAME.glob('*.png'):
             image = util.read_image(str(filename), grayscale=True)
 
             print('Saved image shape:', image.shape)
