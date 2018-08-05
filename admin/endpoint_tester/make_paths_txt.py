144d64bbd27434d084bab2e19a9d7cc517c35410
Arjun Singh <arjun810@gmail.com>
2018-08-05 00:13:54 -0700

    monitoring info

--- admin/endpoint_tester/make_paths_txt.py
+++ admin/endpoint_tester/make_paths_txt.py
@@ -1,6 +1,7 @@
+import sys
 with open('endpoints.txt') as endpoints_file:
     endpoints = [x.strip() for x in endpoints_file.readlines()]
-with open('remote_images.txt') as image_urls_file:
+with open(sys.argv[1]) as image_urls_file:
     remote_image_urls = [x.strip() for x in image_urls_file.readlines()]
 
 paths = []
@@ -8,4 +9,4 @@ for endpoint in endpoints:
     for rem in remote_image_urls:
         s = '{0}/v1/predict?image_url={1}'.format(endpoint, rem)
         paths.append(s)
-        print s
+        print(s)
