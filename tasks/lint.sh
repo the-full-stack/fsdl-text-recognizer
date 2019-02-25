501a44326ad85382ece134da9a5144d8aada0649
Sergey Karayev <sergeykarayev@gmail.com>
2019-02-25 10:08:05 -0800

    adding linting

---
+++ tasks/lint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -euo pipefail
+
+echo "pipenv check"
+pipenv check
+
+# echo "pylint"
+pipenv run pylint api text_recognizer training
+
+echo "pycodestyle"
+pipenv run pycodestyle api text_recognizer training
+
+echo "mypy"
+pipenv run mypy api text_recognizer training
+
+echo "bandit"
+pipenv run bandit -ll -r {api,text_recognizer,training}
+
+echo "shellcheck"
+shellcheck ./**/*.sh
