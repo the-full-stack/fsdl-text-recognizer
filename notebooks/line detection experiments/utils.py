027363ecaded9c54946345610520e147b4855135
Saurabh Bipin Chandra <srbhchandra@gmail.com>
2019-02-27 00:50:22 -0800

    minor
    Signed-off-by: Saurabh Bipin Chandra <srbhchandra@gmail.com>

--- notebooks/line detection experiments/utils.py
+++ notebooks/line detection experiments/utils.py
@@ -1,9 +1,15 @@
+import os
+import random
+import string
 from typing import Dict, List
 import matplotlib as mpl
 # pylint: disable=C0413
 mpl.use('Agg')
 import matplotlib.pyplot as plt  # noqa
 from matplotlib.figure import Figure  # noqa
+
+
+_IDENTIFIER_LENGTH = 8
 
 
 def plot_training_history(history: Dict[str, List[float]], metric: str = 'acc', filename: str = '') -> Figure:
@@ -27,3 +33,20 @@ def plot_training_history(history: Dict[str, List[float]], metric: str = 'acc', 
         ax2.legend()
     if filename:
         plt.savefig(filename)
+
+
+def generate_unique_identifier(dirname_with_identifiers: str, identifier_length: int = _IDENTIFIER_LENGTH) -> str:
+    """
+    dirname_with_identifiers is the path to the directory with sub-directories created with previous identifiers.
+    This function generates and returns an identifier which is not already present in dirname_with_identifiers.
+    """
+    if os.path.exists(dirname_with_identifiers):
+        existing_identifiers = os.listdir(dirname_with_identifiers)
+    else:
+        existing_identifiers = []
+
+    while 1:
+        identifier = ''.join(random.choices(string.ascii_lowercase + string.digits, k=identifier_length))
+        if identifier not in existing_identifiers:
+            break
+    return identifier        
