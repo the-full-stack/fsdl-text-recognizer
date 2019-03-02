5dcc5094ea20fa5d225bceef5d1409271ed7a2a1
Saurabh Bipin Chandra <srbhchandra@gmail.com>
2019-03-02 11:35:50 -0800

    Updating test
    Signed-off-by: Saurabh Bipin Chandra <srbhchandra@gmail.com>

--- text_recognizer/tests/test_paragraph_text_recognizer.py
+++ text_recognizer/tests/test_paragraph_text_recognizer.py
@@ -3,6 +3,7 @@ import os
 from pathlib import Path
 import unittest
 from text_recognizer.paragraph_text_recognizer import ParagraphTextRecognizer
+import text_recognizer.util as util
 
 
 SUPPORT_DIRNAME = Path(__file__).parents[0].resolve() / 'support' / 'iam_paragraphs'
@@ -13,8 +14,13 @@ os.environ["CUDA_VISIBLE_DEVICES"] = ""
 class TestEmnistLinePredictor(unittest.TestCase):
     def test_filename(self):  # pylint: disable=R0201
         predictor = ParagraphTextRecognizer()
-
+        text_offset = 300
         for filename in (SUPPORT_DIRNAME).glob('*.jpg'):
-            predicted_text, line_region_crops = predictor.predict(str(filename))
+            
+            full_image = util.read_image(str(filename), grayscale=True)
+            roi_image = full_image[text_offset:, :]
+            
+            predicted_text, line_region_crops = predictor.predict(roi_image)
             print(predicted_text)
-            assert len(line_region_crops) == 8
+            print(len(line_region_crops))
+            assert len(line_region_crops) == 7
