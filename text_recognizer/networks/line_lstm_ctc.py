315f8f62ae16eedec47b8247c159b6e3cb3b8071
Sergey Karayev <sergeykarayev@gmail.com>
2020-03-29 22:15:16 -0700

    update

--- text_recognizer/networks/line_lstm_ctc.py
+++ text_recognizer/networks/line_lstm_ctc.py
@@ -1,8 +1,6 @@
 """LSTM with CTC for handwritten text recognition within a line."""
-import tensorflow.keras.backend as K
 from tensorflow.python.client import device_lib  # pylint: disable=no-name-in-module
 from tensorflow.keras.layers import Dense, Input, Reshape, TimeDistributed, Lambda, LSTM
-from tensorflow.compat.v1.keras.layers import CuDNNLSTM
 from tensorflow.keras.models import Model as KerasModel
 
 
@@ -23,9 +21,6 @@ def line_lstm_ctc(input_shape, output_shape, window_width=28, window_stride=14):
     y_true = Input(shape=(output_length,), name='y_true')
     input_length = Input(shape=(1,), name='input_length')
     label_length = Input(shape=(1,), name='label_length')
-
-    gpu_present = len(device_lib.list_local_devices()) > 2
-    lstm_fn = CuDNNLSTM if gpu_present else LSTM
 
     # Your code should use slide_window and extract image patches from image_input.
     # Pass a convolutional model over each image patch to generate a feature vector per window.
@@ -49,7 +44,7 @@ def line_lstm_ctc(input_shape, output_shape, window_width=28, window_stride=14):
     convnet_outputs = TimeDistributed(convnet)(image_patches)
     # (num_windows, 128)
 
-    lstm_output = lstm_fn(128, return_sequences=True)(convnet_outputs)
+    lstm_output = LSTM(128, return_sequences=True)(convnet_outputs)
     # (num_windows, 128)
 
     softmax_output = Dense(num_classes, activation='softmax', name='softmax_output')(lstm_output)
